e8526028e153d65d342d560d1f4727dde37168ca
Ivan Boborov <ivanchik.net@mail.ru>
2024-12-13 00:46:06 +0300

    Добавлены тестовые данные

--- conference-program/main-project/main-project.cpp
+++ conference-program/main-project/main-project.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #2. Conference Program.\n";
     cout << "Author: Ivan Bobrov\n";
+    cout << "Group: 24Ping1D\n";
     return 0;
 }
