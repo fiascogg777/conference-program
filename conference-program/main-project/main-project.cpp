e52a94eec4b93f6939140b4f8b9471c038c1b54f
fiascogg777 <114941405+fiascogg777@users.noreply.github.com>
2024-12-12 23:49:27 +0300

    Добавлен вывод общей информации о задании

--- conference-program/main-project/main-project.cpp
+++ conference-program/main-project/main-project.cpp
@@ -1,7 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #2. Conference Program.\n";
+    cout << "Author: Ivan Bobrov\n";
     return 0;
 }
