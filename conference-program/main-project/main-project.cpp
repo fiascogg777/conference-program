99b6a8660d53da65122863bbd972e4bfb70cba0e
Ivan Boborov <ivanchik.net@mail.ru>
2024-12-13 01:18:46 +0300

    Реализована фильтрация данных

--- conference-program/main-project/main-project.cpp
+++ conference-program/main-project/main-project.cpp
@@ -1,50 +1,73 @@
 #include <iostream>
-#include <iomanip>
 #include <vector>
 #include "file_reader.h"
-#include "constants.h"
-#include "Report.h"
+#include "sort.h"
+#include "filter.h"
 
-using namespace std;
+void printReports(const std::vector<Report>& reports) {
+    for (const auto& report : reports) {
+        std::cout << report.start << " " << report.end << " "
+            << report.surname << " " << report.name << " " << report.patronymic
+            << " " << report.topic << "\n";
+    }
+}
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-    cout << "Laboratory work #8. GIT\n";
-    cout << "Variant #2. Conference Program.\n";
-    cout << "Author: Ivan Bobrov\n";
-    cout << "Group: 24Ping1D\n";
-    
-    int size;
-    Report** reports;
-    // Считывание данных из файла
-    read("data.txt", reports, size);
+    setlocale(LC_ALL, "ru");
+    std::vector<Report> reports;
 
-    if (reports.empty()) {
-        cout << "Данные отсутствуют или произошла ошибка при чтении файла.\n";
-        return 1;
+    // Чтение данных из файла
+    reports = FileReader::readFromFile("data.txt");
+
+    int choice;
+
+    std::cout << "Выберите действие:\n";
+    std::cout << "1. Фильтрация по автору (Иванов Иван Иванович)\n";
+    std::cout << "2. Фильтрация по длительности доклада (>15 минут)\n";
+    std::cout << "3. Сортировка\n";
+    std::cin >> choice;
+
+    if (choice == 1) {
+        filterByAuthor(reports, "Ivanov", "Ivan", "Ivanovich");
+    }
+    else if (choice == 2) {
+        filterByDuration(reports, 15);
+    }
+    else if (choice == 3) {
+        int sortMethod, sortCriterion;
+
+        std::cout << "Выберите метод сортировки:\n";
+        std::cout << "1. Пирамидальная сортировка (Heap sort)\n";
+        std::cout << "2. Сортировка слиянием (Merge sort)\n";
+        std::cin >> sortMethod;
+
+        std::cout << "Выберите критерий сортировки:\n";
+        std::cout << "1. По убыванию длительности доклада\n";
+        std::cout << "2. По возрастанию фамилии автора доклада (и темы)\n";
+        std::cin >> sortCriterion;
+
+        if (sortMethod == 1) {
+            if (sortCriterion == 1) {
+                Sort::heapSortByDuration(reports);
+            }
+            else if (sortCriterion == 2) {
+                Sort::heapSortBySurnameAndTopic(reports);
+            }
+        }
+        else if (sortMethod == 2) {
+            if (sortCriterion == 1) {
+                Sort::mergeSortByDuration(reports);
+            }
+            else if (sortCriterion == 2) {
+                Sort::mergeSortBySurnameAndTopic(reports);
+            }
+        }
+
+        // Вывод отсортированных данных
+        std::cout << "Отсортированные доклады:\n";
+        printReports(reports);
     }
 
-    cout << "***** Библиотечный отчет *****\n\n";
-    for (const auto& report : reports) {
-        /********** вывод читателя **********/
-        cout << "Читатель........: ";
-        cout << report.surname << " ";
-        cout << report.name[0] << ". ";
-        cout << report.patronymic[0] << ".\n";
-
-        /********** вывод книги **********/
-        cout << "Тема............: ";
-        cout << '"' << report.topic << "\"\n";
-
-        /********** вывод даты начала **********/
-        cout << "Время начала.....: ";
-        cout << setw(4) << setfill('0') << report.start << '\n';
-            
-
-        /********** вывод даты окончания **********/
-        cout << "Время окончания..: ";
-        cout << setw(2) << setfill('0') << report.end << '\n';
-    }
-
-   
+    return 0;
 }
+
